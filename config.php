ee1611dfaef66c032861adda1ab473efea2236de
Unknown <robinbrongers2001@gmail.com>
2019-04-10 12:19:06 +0200

    Config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 10-4-2019
+ * Time: 12:18
+ */
+
+
+$dbHost = 'localhost';
+$dbUser = 'root';
+$dbPassword = '';
+$dbName = 'F9';
+
+$dsn= "mysql:host=$dbHost;dbname=$dbName";
+$db = new PDO($dsn, $dbUser, $dbPassword);
+
+$db->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
