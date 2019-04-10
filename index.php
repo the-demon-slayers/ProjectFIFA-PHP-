c7d932ba5d6fc0c132e232a28a871fb11fbafed5
Unknown <robinbrongers2001@gmail.com>
2019-04-10 12:35:48 +0200

    Header

--- index.php
+++ index.php
@@ -5,13 +5,17 @@ require 'header.php';
 
 
 <header>
-    <input type="text" id="username" name="username" placeholder="Gebruikersnaam" required>
-    <input type="password" id="password" name="password" placeholder="Wachtwoord" required>
-    <input type="submit" value="Login">
+    <h1>Name_here</h1>
+
+    <div class="login">
+        <input type="text" id="username" name="username" placeholder="Gebruikersnaam" required>
+        <input type="password" id="password" name="password" placeholder="Wachtwoord" required>
+        <input type="submit" value="Login">
+    </div>
 </header>
 
 <div class="banner">
-
+    <h2>Voetbal wedstrijden voor jou bedrijf</h2>
 </div>
 
 
