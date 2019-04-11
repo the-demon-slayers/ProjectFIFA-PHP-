d966432f20d49d9c28658cf2808a226955be8894
Unknown <robinbrongers2001@gmail.com>
2019-04-11 10:01:35 +0200

    Register done

--- register.php
+++ register.php
@@ -10,3 +10,37 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     header('Location: index.php');
     exit;
 }
+
+require 'config.php';
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$password_repeat = $_POST['password_repeat'];
+
+if ($password != $password_repeat){
+    echo 'Is niet gelijk';
+    exit();
+}
+
+$query = "SELECT * FROM users WHERE username=:username";
+$prepare = $db->prepare($query);
+$prepare->execute([
+    ':username' => $username
+]);
+
+$result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+
+if ($result){
+    echo'Deze gebruikersnaam bestaat al';
+    exit();
+}
+
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+$sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+$prepare = $db->prepare($sql);
+$prepare->execute([
+    ':username' =>$username,
+    ':password' =>$hashed_password
+]);
+
+header('Location: index.php');
