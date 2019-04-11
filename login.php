0c6e9d1da5951cf0eae37728114e8694e2a16113
Unknown <robinbrongers2001@gmail.com>
2019-04-11 09:41:06 +0200

    login config

--- login.php
+++ login.php
@@ -5,9 +5,30 @@
  * Date: 10-4-2019
  * Time: 13:50
  */
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    header('Location: index.php');
+    exit;
+}
 
 require 'config.php';
 
 $username = $_POST['username'];
 $password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username=:username";
+$prepare = $db->prepare($sql);
+$prepare->execute([
+    ':username' => $username
+]);
+
+$result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+
+if ($result){
+    $hashed_password = $result[0]['password'];
+    if (password_verify($password, $hashed_password)) {
+        $_SESSION['user'] = $username;
+        header('Location: index.php');
+        exit();
+    }
+}
 
