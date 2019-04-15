a5aede14790b565191bddf521a0b0524c7c6d16b
Unknown <robinbrongers2001@gmail.com>
2019-04-15 10:24:15 +0200

    Made_by

--- add_team.php
+++ add_team.php
@@ -9,15 +9,20 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST'){
     header('Location: index.php');
     exit;
 }
+
+session_start();
 require 'config.php';
 
 $Name = $_POST['team_name'];
+$made_by = $_SESSION['username'];
 
-$sql = "INSERT INTO teams ( team_name ) VALUES ( :name)";
+$sql = "INSERT INTO teams ( team_name, made_by ) VALUES ( :name, :made_by)";
 
 $prepare = $db->prepare($sql);
 $prepare->execute([
-    ':name' => $Name
+    ':name' => $Name,
+    ':made_by' => $made_by
 ]);
+
 
 header('Location: index.php');
