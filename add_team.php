c3b8ed5690ce0ddfe00ba7d08182de1c50688436
Unknown <robinbrongers2001@gmail.com>
2019-05-10 10:35:57 +0200

    Niet mogelijk om twee teams met de dezelfde naam te hebben

--- add_team.php
+++ add_team.php
@@ -16,12 +16,25 @@ require 'config.php';
 $Name = $_POST['team_name'];
 $made_by = $_SESSION['username'];
 
-$sql = "INSERT INTO teams ( team_name, made_by ) VALUES ( :name, :made_by)";
-
-$prepare = $db->prepare($sql);
+$sql1 = "SELECT * FROM teams WHERE team_name=:team_name";
+$prepare = $db->prepare($sql1);
 $prepare->execute([
-    ':name' => $Name,
-    ':made_by' => $made_by
+    ':team_name' => $Name
 ]);
 
-header('Location: index.php');
+$team_name = $prepare->fetch(PDO::FETCH_ASSOC);
+
+if ($team_name == "") {
+
+    $sql = "INSERT INTO teams ( team_name, made_by ) VALUES ( :name, :made_by)";
+
+    $prepare = $db->prepare($sql);
+    $prepare->execute([
+        ':name' => $Name,
+        ':made_by' => $made_by
+    ]);
+
+    header('Location: index.php');
+}else{
+    echo "Dit team bestaat al";
+}
