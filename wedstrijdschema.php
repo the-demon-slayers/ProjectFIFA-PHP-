62daebf3837aca7915f8c7624a6b7b424e6f4af4
Unknown <robinbrongers2001@gmail.com>
2019-05-21 13:56:19 +0200

    wedstrijdschema werkt

--- wedstrijdschema.php
+++ wedstrijdschema.php
@@ -7,23 +7,65 @@
  */
 require 'config.php';
 
-$sql = "SELECT team_name FROM teams";
-$query = $db->query($sql);
-$teams = $query->fetchAll(PDO::FETCH_ASSOC);
-
-$amount_of_teams = count($teams);
-
-echo "$amount_of_teams";
-
-$amount_of_matches = $amount_of_teams * $amount_of_teams - $amount_of_teams;
-$amount_of_matches = $amount_of_matches / 2;
-var_dump($amount_of_matches);
+//$sql = "SELECT team_name FROM teams";
+//$query = $db->query($sql);
+//$teams = $query->fetchAll(PDO::FETCH_ASSOC);
+//
+//$amount_of_teams = count($teams);
+//
+//echo "$amount_of_teams";
+//
+//$amount_of_matches = $amount_of_teams * $amount_of_teams - $amount_of_teams;
+//$amount_of_matches = $amount_of_matches / 2;
+//var_dump($amount_of_matches);
 
 
 require 'header.php';
 ?>
 
+<?php
+require 'Config.php';
+$sql = "SELECT team_name FROM teams;";
+$prepare = $db->prepare($sql);
+$prepare->execute();
+$teams = $prepare->fetchAll(PDO::FETCH_ASSOC);
+$teamsLength = count($teams);
+$minute = 0;
 
+$min = 1;
+$max = 4;
+
+?>
+<table>
+    <tr>
+        <th>Team 1</th>
+        <th>Team 2</th>
+    </tr>
+    <?php
+    foreach ($teams as $team) {
+        $teams = array_slice($teams, 1, $teamsLength);
+        foreach ($teams as $otherTeam) {
+            $teamName = $team['team_name'];
+            $otherTeamName = $otherTeam['team_name'];
+
+
+            ?>
+            <tr>
+            <?php
+
+
+            echo "<td>$teamName</td>";
+            echo "<td>$otherTeamName</td>";
+
+
+        }
+        ?>
+        </tr>
+
+        <?php
+
+    }
+    ?> </table>
 
 
 
