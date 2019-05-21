258a7fc217fdfa64356e76d426ac89ce32bd358c
Unknown <robinbrongers2001@gmail.com>
2019-05-21 11:09:44 +0200

    Dingen

--- wedstrijdschema.php
+++ wedstrijdschema.php
@@ -5,6 +5,14 @@
  * Date: 21-5-2019
  * Time: 10:53
  */
+require 'config.php';
+
+$sql = "SELECT * FROM teams";
+$query = $db->query($sql);
+$teams = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo"<pre>";
+var_dump($teams);
 
 
 require 'header.php';
