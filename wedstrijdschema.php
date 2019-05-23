e8a7527ac5619f05367813d30c23d4df7e8b1ec0
Zaid <thereallombax1337@gmail.com>
2019-05-23 09:23:49 +0200

    Changed the WedstrijdSchema view and made the index list easier to read
    Added a few things to wedstrijdschema.php to make the Team List seem more full. Did the same for the main css. Created a row for Table class.

--- wedstrijdschema.php
+++ wedstrijdschema.php
@@ -6,7 +6,7 @@
  * Time: 10:53
  */
 require 'config.php';
-
+session_start();
 require 'header.php';
 ?>
 
@@ -19,9 +19,39 @@ $teams = $prepare->fetchAll(PDO::FETCH_ASSOC);
 $teamsLength = count($teams);
 
 ?>
+<header>
+     
+    <div class="header-content" id="header">
+            <a href="index.php" class="vertical-align"><img src="img/logo.png" alt="FIFA" class="logo"></a>
+            <?php
+            if (!isset($_SESSION['username'])){
+                echo"
+                <div class='login'>
+                <form action='login.php' method='post'>
+                    <input type='text' id='username' name='username' placeholder='Gebruikersnaam' required>
+                    <input type='password' id='password' name='password' placeholder='Wachtwoord' required>
+                    <input type='submit' value='Login'>
+                </form>
+                <a href='register_form.php' class='register-link'>Maak een account aan</a>       
+            </div>    
+            ";
+            }else{
+                echo "<a href='logout.php'>Uitloggen voor ".$_SESSION['username']."</a>";
+
+                if ($_SESSION['username'] == 'ikbenrobin5') {
+                    echo "<a href='all_profiles.php'>Alle profielen</a>";
+                }
+            }
+            ?>
+        </div>
+    </header>
+   
+</header>
+<div class="background">   
 <table>
     <tr>
         <th>Team 1</th>
+        <th>  </th>
         <th>Team 2</th>
     </tr>
     <?php
@@ -34,12 +64,14 @@ $teamsLength = count($teams);
             <tr>
             <?php
             echo "<td>$teamName</td>";
+            echo "<td> VS. </td>";
             echo "<td>$otherTeamName</td>";
         }
         ?>
         </tr>
         <?php
     }
-    ?> </table>
-
+    ?> 
+</table>
+</div>
 <?php require 'footer.php'; ?>
