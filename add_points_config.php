632165a48c06c882c22f08805fd28750e3eda6f2
Unknown <robinbrongers2001@gmail.com>
2019-06-04 12:31:38 +0200

    Punten toevoegen werkt

--- add_points_config.php
+++ add_points_config.php
@@ -16,7 +16,7 @@ $team2_points = $_POST['team2_points'];
 
 require 'config.php';
 
-$sql = "INSERT INTO games (team1_points, team2_points) VALUES (:team1_points, :team2_points) WHERE (id) = :id";
+$sql = "UPDATE games SET team1_points=:team1_points, team2_points=:team2_points WHERE id=:id ";
 $prepare = $db->prepare($sql);
 $prepare->execute([
     ':team1_points' => $team1_points,
@@ -24,6 +24,6 @@ $prepare->execute([
     ':id' => $game_id
 ]);
 
-//header('Location: wedstrijdschema.php')
+header('Location: wedstrijdschema.php')
 
 ?>
